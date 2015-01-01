be09ae8dddc10e8d1de449c5928c78e0d2e09ec9
liorg <lior.grosman@gmail.com>
2015-01-01 22:57:56 +0200

    AA

---
+++ voteweb/voteweb/Dal/VoteRemark.cs
@@ -0,0 +1,19 @@
+﻿namespace voteweb.Dal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+
+    [Table("kipodeal_admin.VoteRemark")]
+    public class VoteRemark
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid voteremarkid { get; set; }
+
+        public string desc { get; set; }
+
+
+    }
+}
