01c43ca2a989a3441688c296e114d0a4ace41150
liorg <lior.grosman@gmail.com>
2015-01-08 14:47:41 +0200

    ok

--- voteweb/voteweb/Scripts/my/GraphLoad.js
+++ voteweb/voteweb/Scripts/my/GraphLoad.js
@@ -1,5 +1,8 @@
 ﻿
 ang.controller("MainCtrl", function ($scope) {
+    $scope.chartObject = {};
+
+    var style = "padding:0;height:400px";
 
     $scope.Survery = {
         SelectedDate: "2011-2-22",
@@ -14,9 +17,16 @@ ang.controller("MainCtrl", function ($scope) {
                         { Name: "ל", Val: 222 },
                         { Name: "ע", Val: 3 },
                         { Name: "x", Val: 33 },
+                    ],
+                Links:
+                    [
+                        { Name: "ל", Ico: "ל", Desc: "" },
+                        { Name: "ל", Ico: "ע", Desc: "" },
+                        { Name: "ל", Ico: "x", Desc: "" },
                     ]
 
             },
+           
             OtherDates: [
             {
                 DateItem: {
@@ -39,8 +49,6 @@ ang.controller("MainCtrl", function ($scope) {
                                     { Name: "x", Val: 22 },
                                 ]
                 },
-
-
             },
             {
                 DateItem: {
@@ -52,7 +60,6 @@ ang.controller("MainCtrl", function ($scope) {
                                     { Name: "x", Val: 223 },
                                 ]
                 },
-
             }
             ],
         },
@@ -62,11 +69,12 @@ ang.controller("MainCtrl", function ($scope) {
             { Id: 4, Name: "מאגר מוחות" }
         ]
     };
+
     $scope.changeSurvery = function (s) {
         alert(s.Id)
     };
+
     $scope.otherDatePick = function (s) {
-        alert(s.DateItem.Title);
         changeChartObject(s.DateItem);
         $scope.Survery.SelectedDate = s.DateItem.Title;
         refresh();
@@ -86,10 +94,6 @@ ang.controller("MainCtrl", function ($scope) {
         $scope.status.isopen = !$scope.status.isopen;
     };
 
-    $scope.chartObject = {};
-    var style = "padding:0;height:400px";
-
-   
     function changeChartObject(dateItem) {
         $scope.chartObject.options.title = dateItem.Title;
         $scope.chartObject.data.rows = [];
@@ -102,6 +106,7 @@ ang.controller("MainCtrl", function ($scope) {
             })
         }
     };
+
     function loadChartObject() {
         $scope.chartObject.type = "ColumnChart";
         $scope.chartObject.options = { 'title': '' };
@@ -116,35 +121,13 @@ ang.controller("MainCtrl", function ($scope) {
 
     }
 
-    function loadChartObjectx() {
-        $scope.chartObject.data = {
-            "cols": [
-                { id: "t", label: "Topping", type: "string" },
-                { id: "s", label: "קולות", type: "number" }
-            ],
-            "rows": [
-                {
-                    c: [
-                       { v: "MMM" },
-                       { v: 3 }
-                    ]
-                },
-                { c: $scope.onions },
-
-            ]
-        };
-
-        $scope.chartObject.type = "ColumnChart";
-        $scope.chartObject.options = {
-            'title': 'How Much Pizza I Ate Last Night'
-        }
-    }
-
     function refresh() {
 
         $scope.chart = $scope.chartObject;
     }
+
     loadChartObject();
+
     refresh();
-    $scope.chart.cssStyle = style;
+
 });
