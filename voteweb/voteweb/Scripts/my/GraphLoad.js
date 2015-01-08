d0fb4d8729bb4f2d0a5a1779090a57ada9103e58
liorg <lior.grosman@gmail.com>
2015-01-08 13:32:45 +0200

    show rony

--- voteweb/voteweb/Scripts/my/GraphLoad.js
+++ voteweb/voteweb/Scripts/my/GraphLoad.js
@@ -1,15 +1,59 @@
 ﻿
 ang.controller("MainCtrl", function ($scope) {
-   $scope.Survery = {
+
+    $scope.Survery = {
+        SelectedDate: "2011-2-22",
         Current: {
             Id: '{E7BE131D-321F-4F84-A4D2-709C5C65E421}',
             Name: "דחף",
             Title: "דחף",
-            LastDate: '2011-2-22',
+            LastDate: {
+                Title: '2011-2-22',
+                Items:
+                    [
+                        { Name: "ל", Val: 222 },
+                        { Name: "ע", Val: 3 },
+                        { Name: "x", Val: 33 },
+                    ]
+
+            },
             OtherDates: [
-             '2011-1-1',
-             '2010-1-1',
-             '2009-1-1'
+            {
+                DateItem: {
+                    Title: '2011-2-22',
+                    Items:
+                        [
+                            { Name: "ל", Val: 222 },
+                            { Name: "ע", Val: 3 },
+                            { Name: "x", Val: 33 },
+                        ]
+                },
+            },
+            {
+                DateItem: {
+                    Title: '2011-1-1',
+                    Items:
+                                [
+                                    { Name: "ל", Val: 432 },
+                                    { Name: "ע", Val: 3 },
+                                    { Name: "x", Val: 22 },
+                                ]
+                },
+
+
+            },
+            {
+                DateItem: {
+                    Title: '2091-1-1',
+                    Items:
+                                [
+                                    { Name: "ל", Val: 1 },
+                                    { Name: "ע", Val: 32 },
+                                    { Name: "x", Val: 223 },
+                                ]
+                },
+
+            }
             ],
         },
         AllItems: [
@@ -18,12 +62,14 @@ ang.controller("MainCtrl", function ($scope) {
             { Id: 4, Name: "מאגר מוחות" }
         ]
     };
-    $scope.changeSurvery = function (s)
-    {
+    $scope.changeSurvery = function (s) {
         alert(s.Id)
     };
     $scope.otherDatePick = function (s) {
-        alert(s); 
+        alert(s.DateItem.Title);
+        changeChartObject(s.DateItem);
+        $scope.Survery.SelectedDate = s.DateItem.Title;
+        refresh();
     };
 
     $scope.status = {
@@ -43,31 +89,62 @@ ang.controller("MainCtrl", function ($scope) {
     $scope.chartObject = {};
     var style = "padding:0;height:400px";
 
-    $scope.onions = [
-        { v: "Onions" }, { v: 3 }
-    ];
-
-    $scope.chartObject.data = {
-        "cols": [
-            { id: "t", label: "Topping", type: "string" },
-            { id: "s", label: "קולות", type: "number" }
-        ], "rows": [
-            {
+   
+    function changeChartObject(dateItem) {
+        $scope.chartObject.options.title = dateItem.Title;
+        $scope.chartObject.data.rows = [];
+        for (row in dateItem.Items) {
+            $scope.chartObject.data.rows.push({
                 c: [
-                   { v: "MMM" },
-                   { v: 3 }
+                   { v: dateItem.Items[row].Name },
+                   { v: dateItem.Items[row].Val }
                 ]
-            },
-            { c: $scope.onions },
-
-        ]
+            })
+        }
     };
+    function loadChartObject() {
+        $scope.chartObject.type = "ColumnChart";
+        $scope.chartObject.options = { 'title': '' };
+        $scope.chartObject.data = {
+            "cols": [
+                { id: "t", label: "Topping", type: "string" },
+                { id: "s", label: "קולות", type: "number" }
+            ],
+            "rows": []
+        };
+        changeChartObject($scope.Survery.Current.LastDate);
 
-    $scope.chartObject.type = "ColumnChart";// $routeParams.chartType;
-    $scope.chartObject.options = {
-        'title': 'How Much Pizza I Ate Last Night'
     }
 
-    $scope.chart = $scope.chartObject;
+    function loadChartObjectx() {
+        $scope.chartObject.data = {
+            "cols": [
+                { id: "t", label: "Topping", type: "string" },
+                { id: "s", label: "קולות", type: "number" }
+            ],
+            "rows": [
+                {
+                    c: [
+                       { v: "MMM" },
+                       { v: 3 }
+                    ]
+                },
+                { c: $scope.onions },
+
+            ]
+        };
+
+        $scope.chartObject.type = "ColumnChart";
+        $scope.chartObject.options = {
+            'title': 'How Much Pizza I Ate Last Night'
+        }
+    }
+
+    function refresh() {
+
+        $scope.chart = $scope.chartObject;
+    }
+    loadChartObject();
+    refresh();
     $scope.chart.cssStyle = style;
 });
